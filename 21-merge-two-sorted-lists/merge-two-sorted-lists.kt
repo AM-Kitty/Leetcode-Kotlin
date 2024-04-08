ba68c4e4b02c0667d7db34771ff2843034af5b2c
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-04-08 16:35:38 -0400

    Time: 162 ms (59.49%) | Memory: 35.3 MB (82.32%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.kt
@@ -0,0 +1,43 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        val dummyHead = ListNode(0)
+        // use the current pointer to move to NEXT node
+        var cur = dummyHead
+
+        var node1 = list1
+        var node2 = list2
+
+        while(node1 != null || node2 != null){
+
+            if (node1 == null){
+                cur.next = node2
+                break
+            }else if (node2 == null){
+                cur.next = node1
+                break
+            }
+            
+            if (node1.`val` < node2.`val`){
+                cur.next = node1 
+                node1 = node1.next
+            } else {
+                cur.next = node2 
+                node2 = node2.next
+            }
+
+            cur = cur.next
+        }
+
+        return dummyHead?.next
+        
+    }
+}
