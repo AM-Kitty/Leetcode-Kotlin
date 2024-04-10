0d3ea2232ba32bb06c8db8cff903c0233a79428c
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-04-10 15:53:07 -0400

    Time: 278 ms (12.90%) | Memory: 39.4 MB (66.67%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.kt
@@ -0,0 +1,116 @@
+data class Node(
+    val value: Int, 
+    var next: Node? = null
+)
+
+class MyLinkedList() {
+    
+    private var cur: Node? = null
+    private var head: Node? = null
+    
+
+    private var i = 0
+    private var size = 0
+
+    fun get(index: Int): Int {
+        var cur_idx = 0
+        if (head == null || index >= size){
+            return -1
+        }
+
+        cur = head
+
+        while(cur_idx != index && cur?.next != null){
+            cur_idx ++
+            cur = cur?.next
+        }
+        // return cur?.value ?: -1
+        if (cur_idx == index){
+            return cur?.value ?: -1
+        } else {
+            return -1
+        }
+    }
+    //= getNode(index)?.value ?: -1
+
+    private fun getNode(index: Int): Node? {
+        i=0
+        cur = head
+        while(i++ < index && cur?.next != null) {
+            cur = cur?.next
+        }
+        if(--i==index) return cur
+        else return null
+    }
+
+    fun addAtHead(`val`: Int) {
+        if(head == null) head = Node(`val`)
+        else {
+            cur = head
+            head = Node(`val`, next = cur)
+        }
+
+        size++
+    }
+
+    fun addAtTail(`val`: Int) {
+        if(head == null) {
+            addAtHead(`val`)
+            return
+        }
+        
+        cur = head
+        while (cur?.next != null) cur = cur?.next
+
+        cur?.next = Node(`val`)
+
+        size++
+    }
+
+    fun addAtIndex(index: Int, `val`: Int) {
+        if(index > size || index < 0) return
+        if(index == 0) {
+            addAtHead(`val`)
+            return
+        }
+
+        cur = head
+        var cur_idx = 0
+
+        while(cur_idx != index-1 && cur?.next != null){
+            cur_idx ++
+            cur = cur?.next
+        }
+
+        // getNode(index-1)?.let {
+        //     cur = it
+        //     it.next = Node(`val`, next = cur?.next)
+        // }
+        val temp = cur
+        cur?.next = Node(`val`, next = temp?.next)
+
+        size++
+    }
+
+    fun deleteAtIndex(index: Int) {
+        if(size == 0 || index > size || index < 0) return
+        if(index == 0) {
+            head = head?.next
+            return
+        }
+
+        cur = head
+        var cur_idx = 0
+        while(cur_idx != index-1 && cur?.next != null){
+            cur_idx ++
+            cur = cur?.next
+        }
+        cur?.next = cur?.next?.next
+
+
+
+        // getNode(index-1)?.let {
+        //     it.next = it.next?.next
+        // }
+    }
+}
