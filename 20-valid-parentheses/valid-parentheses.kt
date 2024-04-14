87e73489a90d5f5a305a62bd6dd2a0d42fc6dfc5
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-04-14 18:06:27 -0400

    Time: 127 ms (96.46%) | Memory: 34 MB (77.80%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.kt
+++ 20-valid-parentheses/valid-parentheses.kt
@@ -1,26 +1,19 @@
 class Solution {
     fun isValid(s: String): Boolean {
-        // Keywords: LIFO (stack) -> ArrayDeque
-        if (s.length % 2 !=0){
-            return false
-        }
-
-        val map = mapOf(
+        val charMap = mapOf(
             ')' to '(',
-            '}' to '{',
-            ']' to '['
+            ']' to '[',
+            '}' to '{'
         )
-        val stack = ArrayDeque<Char>()
 
-        for(c in s){
-            val openBracket = map[c]
-
-            if (openBracket != null && stack.isNotEmpty() && stack.last() == openBracket){
-                stack.removeLast()
+        val stack = Stack<Char>()
+        for (c in s){
+            val openBraket = charMap[c]
+            if (openBraket != null && stack.isNotEmpty() && stack.peek() == openBraket){
+                stack.pop()
             } else {
-                stack.add(c)
+                stack.push(c)
             }
-
         }
 
         return stack.isEmpty()
