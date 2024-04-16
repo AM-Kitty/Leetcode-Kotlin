1e40053261e304fb36cdef5a98be67d23bdfb957
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-04-16 17:16:28 -0400

    Time: 270 ms (90.82%) | Memory: 40.6 MB (17.35%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.kt
@@ -0,0 +1,38 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    // Keywords: Recursion, BST
+
+    var result: TreeNode? = null
+
+    fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+        val insertNode = TreeNode(`val`)
+        if (root == null) return insertNode
+        insert(root, insertNode)
+        return root
+    }
+
+    private fun insert(root: TreeNode, insertNode: TreeNode){
+        if (insertNode.`val` < root.`val`) {
+            root.left?.let {
+                insert(it, insertNode)
+            } ?: run {
+                root.left = insertNode
+            }
+        } else {
+            root.right?.let {
+                insert(it, insertNode)
+            } ?: run {
+                root.right = insertNode
+            }
+        } 
+    }
+}
