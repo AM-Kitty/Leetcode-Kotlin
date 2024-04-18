b203c47683ae784e2fa853abfe2d34cedc4d8393
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-04-18 10:24:30 -0400

    Time: 182 ms (67.35%) | Memory: 36.5 MB (100.00%) - LeetSync

---
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun findLengthOfLCIS(nums: IntArray): Int {
+        if(nums.size == 0) return 0
+        var res = 1
+        var count = 1
+        
+        for (i in nums.indices){
+            if ((i+1) < nums.size){
+                if (nums[i+1] > nums[i]){
+                    count ++
+                    res = maxOf(res, count)
+                } else {
+                    count = 1
+                }
+            }
+        }
+        return res
+    }
+}
